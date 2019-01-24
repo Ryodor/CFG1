a6aabaf3c4ee600bf53842161659d892296a278b
BounaixVincent <bounaixvincent@gmail.com>
2019-01-24 10:03:33 +0100

    bug fixed. works fine

--- browsefiles.py
+++ browsefiles.py
@@ -9,12 +9,12 @@ def browse_through_files():
     for root, dirs, files in os.walk(".", topdown=False):
         for name in files:
             list_of_files.append(name)
-            return list_of_files
-
+    return list_of_files
 
 
 def main():
     list = browse_through_files()
+    print(list)
 
 
 if __name__ == '__main__':
