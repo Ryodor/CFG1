e1f6a16e5c610a58060767a5a64dcc808e89e3b7
BounaixVincent <bounaixvincent@gmail.com>
2019-01-24 09:48:24 +0100

    function that see through all files and get their name. Not fully done

---
+++ browsefiles.py
@@ -0,0 +1,21 @@
+import os
+
+def browse_through_files():
+    """
+    :return:
+        list_of_files ([]): A list filled with the name of each files
+    """
+    list_of_files = []
+    for root, dirs, files in os.walk(".", topdown=False):
+        for name in files:
+            list_of_files.append(name)
+            return list_of_files
+
+
+
+def main():
+    list = browse_through_files()
+
+
+if __name__ == '__main__':
+    main()
