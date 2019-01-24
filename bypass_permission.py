69bbcc2b82986c7c5c41da3f46613bd611c2e118
BounaixVincent <bounaixvincent@gmail.com>
2019-01-24 10:50:40 +0100

    changes chmod with a parameter

--- bypass_permission.py
+++ bypass_permission.py
@@ -1,17 +1,15 @@
 import os
 
-def bypass():
-    """This method change the rights of every file to 777
+def bypass(path):
+    """This method changes the rights of every file to 777
 
     :return:
     """
-    for root, dirs, files in os.walk(".", topdown=False):
-        for name in files:
-            os.chmod("../../Documents/" + name, 511)
+    os.chmod(path, 511)
 
 
 def main():
-    bypass()
+    bypass("../../Documents/punpun.jpg")
 
 
 if __name__ == '__main__':
